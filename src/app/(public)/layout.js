36ab56fe680fe30d63db286c16d76e1e29d83be1
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-07-11 13:13:10 +0600

    installed better auth, framer-motion, mongodb, recharts, react-toastify, mongo-adapter dependencies

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,13 +1,13 @@
 import Footer from '@/components/public/shared/footer';
 import Navbar from '@/components/public/shared/navbar/navbar';
 import TopHeader from '@/components/public/shared/topHeader';
-import header from '@/components/public/shared/header';
 import React from 'react';
+// import Header from '../../components/public/shared/header';
 
 const layout = ({ children }) => {
   return (
     <div>
-    <header/>
+      {/* <Header /> */}
       <TopHeader />
       <Navbar />
       {children}
