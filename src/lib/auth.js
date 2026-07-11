6c9b181788d8880366c1c040c24b87c0cf0db7e9
Sheikh Muzammil <discordsstudent@gmail.com>
2026-07-11 17:34:38 +0600

    cleared unnassery function from auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,9 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("StayNest");
+const db = client.db("aimhabiganj");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
@@ -29,33 +28,23 @@ export const auth = betterAuth({
     },
   },
 
-  databaseHooks: {
-    user: {
-      create: {
-        before: async (user) => {
-          // যদি ইউজার সোশ্যাল অ্যাকাউন্ট (Google) দিয়ে সাইন-আপ করে
-          // Better-Auth-এ সোশ্যাল সাইন-আপের সময় সাধারণত পাসওয়ার্ড থাকে না বা ইউজার অবজেক্টে নির্দিষ্ট ফ্ল্যাগ থাকে
-          if (!user.password) {
-            return {
-              data: {
-                ...user,
-                role: "tenant",
-              },
-            };
-          }
-          return { data: user };
-        },
-      },
-    },
-  },
-  session: {
-    cookieCache: {
-      enabled: true,
-      strategy: "jwt",
-      maxAge: 60 * 24 * 30,
-    }
-  },
-  plugins: [
-    jwt()
-  ]
+  // databaseHooks: {
+  //   user: {
+  //     create: {
+  //       before: async (user) => {
+     
+  //         if (!user.password) {
+  //           return {
+  //             data: {
+  //               ...user,
+  //               role: "tenant",
+  //             },
+  //           };
+  //         }
+  //         return { data: user };
+  //       },
+  //     },
+  //   },
+  // },
+  
 });
