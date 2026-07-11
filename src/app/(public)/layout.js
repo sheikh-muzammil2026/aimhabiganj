f922313c9033694a900740db184ee520f7368b9a
Sheikh Muzammil <discordsstudent@gmail.com>
2026-07-11 06:22:36 +0600

    Added header

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,11 +1,13 @@
 import Footer from '@/components/public/shared/footer';
 import Navbar from '@/components/public/shared/navbar/navbar';
 import TopHeader from '@/components/public/shared/topHeader';
+import header from '@/components/public/shared/header';
 import React from 'react';
 
 const layout = ({ children }) => {
   return (
     <div>
+    <header/>
       <TopHeader />
       <Navbar />
       {children}
