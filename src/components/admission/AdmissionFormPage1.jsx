c75d6c09d991d05e9e2eb34ce92170cc8ed75f26
Sheikh Muzammil <discordsstudent@gmail.com>
2026-08-19 23:40:31 -0700

    Use environment variable for IMGBB API key

--- src/components/admission/AdmissionFormPage1.jsx
+++ src/components/admission/AdmissionFormPage1.jsx
@@ -110,7 +110,7 @@ export default function AdmissionFormPage1({ formData, handleChange, handleNeste
     data.append("image", file);
 
     try {
-      const apiKey = "5a4f8c279ddcedf0d73f50444bad88b0";
+      const apiKey = `${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`;
       const response = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}`, {
         method: "POST",
         body: data,
